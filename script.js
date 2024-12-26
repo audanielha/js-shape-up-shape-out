08d48f06c8d76235d954d15d8100ba3cb2483158
Daniel Ha <audanielha@gmail.com>
2024-12-26 18:31:35 +1300

    add remaining shape functions

--- script.js
+++ script.js
@@ -17,7 +17,12 @@ let circleHeightWidth = document.querySelector("#circleHeightWidth");
 let triangleSubmit = document.querySelector("#triangleSubmit");
 let triangleHeightWidth = document.querySelector("#triangleHeightWidth");
 
-console.log(circleHeightWidth)
+let shapeNameInput = document.querySelector("#shapeNameInput");
+let heightInput = document.querySelector("#heightInput");
+let widthInput = document.querySelector("#widthInput");
+let areaInput = document.querySelector("#areaInput");
+let radiusInput = document.querySelector("#radiusInput");
+let perimeterInput = document.querySelector("#perimeterInput");
 
 class Shape{
     constructor(height, width){
@@ -32,25 +37,57 @@ class Shape{
         this.div.style.left = `${this.randomNumber()}px`;
         this.div.style.top = `${this.randomNumber()}px`;
         shapeContainer.appendChild(this.div);
+
+        this.div.addEventListener("click",()=>{
+            this.describe()
+        });
+
+        this.div.addEventListener("dblclick", ()=>{
+            this.div.remove()
+        })
     }
 
     randomNumber(){
         return Math.floor(Math.random() * 400)
     }
+    describe(){
+        heightInput.value = `${this.height}`;
+        widthInput.value = `${this.width}`;
+        radiusInput.value = `N/A`;
+        perimeterInput.value = `${this.height * 4}`
+    }
 }
-
+class Rectangle extends Shape{
+    describe(){
+        shapeNameInput.value = "Rectangle";
+        perimeterInput.value = `${2 * (this.height + this.width)}`
+        super.describe()
+    }
+}
 class Square extends Shape{
     constructor(height){
         super(height, height);
-        this.div.classList = "blue";
+        this.div.classList = "red";
+    }
+    describe(){
+        shapeNameInput.value = "Square";
+        areaInput.value = `${this.width * this.width}`;
+        super.describe()
     }
 }
 
 class Circle extends Shape{
     constructor(height){
         super(height, height)
-        this.div.classList = "orange";
+        this.div.classList = "purple";
         this.div.style.borderRadius="50%";
+    }
+    describe(){
+        shapeNameInput.value = "Circle";
+        super.describe()
+        areaInput.value =`${this.width/2 * this.width/2 * 3.14}`;
+        radiusInput.value =`${this.height/2}`;
+        perimeterInput.value = `${2 * 3.14 * this.height/2}`
     }
 }
 
@@ -58,13 +95,20 @@ class Triangle extends Shape{
     constructor(x){
         super(x, x)
         this.div.classList.remove("green");
-        this.div.style.borderLeft= `${x}px solid transparent`;
-        this.div.style.borderRight = `${x}px solid transparent`;
-        this.div.style.borderBottom = `${x * 2}px solid yellow`;
+        this.div.style.borderLeft= `${x/2}px solid transparent`;
+        this.div.style.borderRight = `${x/2}px solid transparent`;
+        this.div.style.borderBottom = `${x}px solid yellow`;
+    }
+    describe(){
+        shapeNameInput.value = "Triangle";
+        perimeterInput.value =`${(2 * this.height + this.height * Math.sqrt(2)).toFixed(2)}`
+        super.describe()
     }
 }
+
+
 rectangleButton.addEventListener("click", ()=>{
-    new Shape(rectangleHeight.value,rectangleWidth.value)
+    new Rectangle(rectangleHeight.value,rectangleWidth.value)
     rectangleHeight.value = " ";
     rectangleWidth.value = " ";
 });
